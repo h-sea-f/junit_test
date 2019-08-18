710e3e46af866b8bcde2039806bd3a6a9af94d46
h-sea-f <h_sea_f@163.com>
2019-08-18 15:58:50 +0800

    finsih should_return_false_when_call_isDateValid_given_sale test

--- src/test/java/sales/SalesAppTest.java
+++ src/test/java/sales/SalesAppTest.java
@@ -2,13 +2,27 @@ package sales;
 
 import org.junit.Test;
 
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
 public class SalesAppTest {
 
+//	@Test
+//	public void testGenerateReport() {
+//
+//		SalesApp salesApp = new SalesApp();
+//		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
+//
+//	}
 	@Test
-	public void testGenerateReport() {
-		
-		SalesApp salesApp = new SalesApp();
-		salesApp.generateSalesActivityReport("DUMMY", 1000, false, false);
-		
+	public void should_return_false_when_call_isDateValid_given_sale(){
+		SalesApp salesApp = spy(new SalesApp());
+		Sales sales = mock(Sales.class);
+		when(sales.getEffectiveTo()).thenReturn(new Date());
+		assertEquals(false,salesApp.isDateValid(sales));
 	}
 }
